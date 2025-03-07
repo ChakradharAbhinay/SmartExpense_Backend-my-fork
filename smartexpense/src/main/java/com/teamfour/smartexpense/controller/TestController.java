f8d4e429e0cfe59b42023b87241b2b1177188397
gauthamkris7neu <venkatakrishnapras.g@northeastern.edu>
2025-03-07 10:27:32 -0500

    Initial Commit

---
+++ smartexpense/src/main/java/com/teamfour/smartexpense/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.teamfour.smartexpense.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        return "SmartExpense API is running!";
+    }
+}
